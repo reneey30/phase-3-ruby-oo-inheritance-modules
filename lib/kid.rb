b65fab2e1776497a127929f5e2ba45e3b73e4bd1
Irene Njeru <irenenjeru8@gmail.com>
2022-06-15 12:33:35 +0300

    task completed

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,16 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+class Kid
+
+    extend MetaDancing
+
+include Dance
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+    # class Kid
+    #     extend MetaDancing
+    # end
+end
